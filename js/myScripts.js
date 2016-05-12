f3e652df7946f6fe422d023efa5f5cce9d9843bd
GabrielXu <ShuboXu@stmarksschool.org>
2016-05-12 09:24:53 -0400

    May 12th

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-alert("hi");
     $("#Tw").hover( function(){
     	
 
@@ -38,3 +37,64 @@ alert("hi");
     	$("#Pi").css("background-color","#ffe6ff");
     });  
 });
+var picList= new Array(6);
+var locationList = new Array(6);
+var captionList = new Array(6);
+
+var fButtonListener;
+var bButtonListener;
+
+
+var whichPic = 0;
+
+
+picList[0] = "images/1.JPG";
+picList[1] = "images/2.JPG";
+picList[2] = "images/3.JPG";
+picList[3] = "images/4.JPG";
+picList[4] = "images/5.JPG";
+picList[5] = "images/6.JPG";
+
+
+captionList[0] = "Hans Wu ";
+captionList[1] = "Luc Cote";
+captionList[2] = "A";
+captionList[3] = "B";
+captionList[4] = "C";
+captionList[5] = "D";
+
+
+
+
+window.onload = function(){
+
+
+document.getElementById("photos").src = picList[whichPic];
+document.getElementById("caption").innerHTML = captionList[whichPic];
+
+fButtonListener = document.getElementById("forwardButton");
+bButtonListener = document.getElementById("backButton");
+
+fButtonListener.addEventListener('click',function(event){
+    //alert("don't poke me")
+    ++whichPic;
+    if (whichPic >= picList.length){
+        whichPic = 0;
+    }
+    document.getElementById("photos").src = picList[whichPic];
+
+document.getElementById("caption").innerHTML = captionList[whichPic];
+});
+bButtonListener.addEventListener('click',function(event){
+    //alert("don't poke me either")
+    --whichPic;
+    if (whichPic < 0){
+        whichPic = picList.length-1;
+    }
+    document.getElementById("photos").src = picList[whichPic];
+
+    document.getElementById("caption").innerHTML = captionList[whichPic];
+});
+
+
+}
