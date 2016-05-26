d54a856e6ce2f0ca7de8c34e009140656da9a9cb
GabrielXu <ShuboXu@stmarksschool.org>
2016-05-26 19:52:54 -0400

    May 26th

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,100 +1,90 @@
+
+
+
+
 $(document).ready(function() {
 
-    $("#Tw").hover( function(){
-    	
+    $(".LOGO").mouseover( function(){
+        
+        $('.dropdown').css("float","right");
+        $(".LOGO").css("width","20%");
+    });
 
-    	$("#Tw").css("background-color","white");
+     $(".LOGO").mouseout(function(){
+
+        $(".LOGO").css("width","10%");
+        $('.dropdown').css("float","none");
+    });
+
+
+    $("#Tw").hover( function(){
+        
+
+        $("#Tw").css("background-color","white");
     });
      $("#Tw").mouseout( function(){
 
-    	$("#Tw").css("background-color","#ffe6ff");
+        $("#Tw").css("background-color","#ffe6ff");
     });
      $("#Fa").hover( function(){
-    	
+        
 
-    	$("#Fa").css("background-color","white");
+        $("#Fa").css("background-color","white");
     });
      $("#Fa").mouseout( function(){
 
-    	$("#Fa").css("background-color","#ffe6ff");
+        $("#Fa").css("background-color","#ffe6ff");
     });
      $("#In").hover( function(){
-    	
+        
 
-    	$("#In").css("background-color","white");
+        $("#In").css("background-color","white");
     });
      $("#In").mouseout( function(){
 
-    	$("#In").css("background-color","#ffe6ff");
+        $("#In").css("background-color","#ffe6ff");
     });
     $("#Pi").hover( function(){
-    	
+        
 
-    	$("#Pi").css("background-color","white");
+        $("#Pi").css("background-color","white");
     });
      $("#Pi").mouseout( function(){
 
-    	$("#Pi").css("background-color","#ffe6ff");
+        $("#Pi").css("background-color","#ffe6ff");
     });  
+
+        $("#AU").hover( function(){
+        
+
+        $("#AU").css("color","#ffe6ff");
+    });
+     $("#AU").mouseout( function(){
+
+        $("#AU").css("color","white");
+    });
+     
+
+     $("#PW").hover( function(){
+        
+
+        $("#PW").css("color","#ffe6ff");
+    });
+     $("#PW").mouseout( function(){
+
+        $("#PW").css("color","white");
+    });
+     $("#CF").hover( function(){
+        
+
+        $("#CF").css("color","#ffe6ff");
+    });
+     $("#CF").mouseout( function(){
+
+        $("#CF ").css("color","white");
+    });
+
+    
+
+      
 });
-var picList= new Array(6);
-var locationList = new Array(6);
-var captionList = new Array(6);
-
-var fButtonListener;
-var bButtonListener;
-
-
-var whichPic = 0;
-
-
-picList[0] = "images/1.JPG";
-picList[1] = "images/2.JPG";
-picList[2] = "images/3.JPG";
-picList[3] = "images/4.JPG";
-picList[4] = "images/5.JPG";
-picList[5] = "images/6.JPG";
-
-
-captionList[0] = "Hans Wu ";
-captionList[1] = "Luc Cote";
-captionList[2] = "A";
-captionList[3] = "B";
-captionList[4] = "C";
-captionList[5] = "D";
-
-
-
-
-window.onload = function(){
-
-
-document.getElementById("photos").src = picList[whichPic];
-document.getElementById("caption").innerHTML = captionList[whichPic];
-
-fButtonListener = document.getElementById("forwardButton");
-bButtonListener = document.getElementById("backButton");
-
-fButtonListener.addEventListener('click',function(event){
-    //alert("don't poke me")
-    ++whichPic;
-    if (whichPic >= picList.length){
-        whichPic = 0;
-    }
-    document.getElementById("photos").src = picList[whichPic];
-
-document.getElementById("caption").innerHTML = captionList[whichPic];
-});
-bButtonListener.addEventListener('click',function(event){
-    //alert("don't poke me either")
-    --whichPic;
-    if (whichPic < 0){
-        whichPic = picList.length-1;
-    }
-    document.getElementById("photos").src = picList[whichPic];
-
-    document.getElementById("caption").innerHTML = captionList[whichPic];
-});
-
-
-}
